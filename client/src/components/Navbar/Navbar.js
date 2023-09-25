78e65757f156863b47508e219f512d04e942d8bf
Harsha <harshitharsha778@gmail.com>
2023-09-25 11:00:30 +0530

    Navbar Nabr commit

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     const dispatch=useDispatch();
     const location=useLocation();
 
-    console.log(user);
+    // console.log(user);
     const logout=()=>{
       dispatch({type:"LOGOUT"});
       navigate("/");
@@ -28,7 +28,7 @@ const Navbar = () => {
     useEffect(()=>{
       const token=user?.token;
 
-
+      
       setuser(JSON.parse(localStorage.getItem("profile")));
 
     },[location])
@@ -50,8 +50,8 @@ const Navbar = () => {
         <Toolbar className={classes.toolbar}>
         {user? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+            <Avatar className={classes.purple} alt={user.result?.name} src={user.result?.imageUrl}>{user.result?.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user.result?.name}</Typography>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
