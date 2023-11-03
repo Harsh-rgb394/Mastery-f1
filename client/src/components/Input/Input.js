c1770bdadec193b2084c5ecb9c0af98e1fd625ab
Harsha <harshitharsha778@gmail.com>
2023-11-03 12:28:20 +0530

    input changes

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -3,12 +3,13 @@ import { TextField,InputAdornment,Grid,IconButton } from '@material-ui/core'
 import Visbility from "@material-ui/icons/Visibility"
 import VisbilityOff from "@material-ui/icons/VisibilityOff"
 
-const Input = ({half,name,label,handleChange,autoFocus,type,handleshowpassword,handleshowconfirmpassword}) => {
+const Input = ({half,name,label,handleChange,autoFocus,type,handleshowpassword}) => {
   return (
     <Grid item xs={12} sm={half? 6:12}>
         <TextField
         name={name}
         label={label}
+        
         onChange={handleChange}
         variant='outlined'
         required
